39c68c45b7d92605c87c92acb82a06812a8c699b
EGrebowski <ellie.grebowski@gmail.com>
2017-12-04 20:45:06 -0500

    show/hide sections: navbar

--- public/client.js
+++ public/client.js
@@ -1,1 +1,57 @@
+//Handle navbar links
+//Navigate to dashboard from navbar
+$('#nav-dashboard').on("click", function (event) {
+    $('.header').hide();
+    $(".search-results").hide();
+    $(".my-profile").hide();
+    $(".new-releases-full").hide();
+    $('.dashboard').show();
+});
 
+// navigate to search page from navbar
+$('#nav-search').on("click", function (event) {
+    $('.dashboard').hide();
+    $('.header').hide();
+    $(".my-profile").hide();
+    $(".new-releases-full").hide();
+    $(".search-results").show();
+});
+
+// navigate to profile from navbar
+$('#nav-profile').on("click", function (event) {
+    $('.header').hide();
+    $('.dashboard').hide();
+    $(".search-results").hide();
+    $(".new-releases-full").hide();
+    $(".my-profile").show();
+});
+
+// navigate to new releases from navbar
+$('#nav-new-releases').on("click", function (event) {
+    $('.header').hide();
+    $('.dashboard').hide();
+    $(".search-results").hide();
+    $(".my-profile").hide();
+    $(".new-releases-full").show();
+});
+
+$('#nav-search').on("click", function (event) {
+    $('.header').hide();
+    $('.dashboard').show();
+});
+
+$('#dashboard-search').on("submit", function (event) {
+    event.preventDefault();
+    $('.dashboard').hide();
+    $('.search-results').show();
+    console.log("test");
+});
+
+$('.book-entry').on("")
+
+$(document).ready(function (event) {
+    //        $(".dashboard").hide();
+    //        $(".search-results").hide();
+    //        $(".my-profile").hide();
+    //        $(".new-releases-full").hide();
+});
